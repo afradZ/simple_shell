bde60f2362d8d365d5c9ff38f69d2f52ec3d2549
afradZ <afradzakaria@gmail.com>
2024-08-16 07:45:40 -0700

    string functions params

---
+++ str_functions.c
@@ -0,0 +1,72 @@
+#include "shell.h"
+
+/**
+ * _strlen -returns length of a string
+ * @s: string to check 
+ *
+ * Return: int length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - performs a comparison of two strings
+ * @s1: first str
+ * @s2: second str
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2g
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 -*s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - check if the needle starts with haystack
+ * @haystack: string to search
+ * @needle: substring to be found
+ *
+ * Return: adress of the next char of haystack or NULL
+ */
+char *starts_with(const char *haystack, const chaar *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination buffer
+ * @src: source buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest) dest++;
+	while (*src)
+		*dest++ = *src ++;
+	*dest = *src;
+	return (ret);
+}
