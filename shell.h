4626d86a24d41dabd450440f79c07fd9f36061ec
afradZ <afradzakaria@gmail.com>
2024-08-20 13:35:06 -0700

    added history file

--- shell.h
+++ shell.h
@@ -188,4 +188,11 @@ char **get_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
 
+/* history.c */
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+
 #endif
