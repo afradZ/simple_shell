3d5b8655b954e6ff235742151e075bfd4677a799
afradZ <afradzakaria@gmail.com>
2024-08-20 09:34:14 -0700

    environment functions

--- shell.h
+++ shell.h
@@ -176,4 +176,16 @@ void clear_info(info_t *);
 void set_info(info_t *, char **);
 void free_info(info_t *, int);
 
+/* env.c */
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
+
+/* environ.c */
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
+
 #endif
